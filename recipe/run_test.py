cdff949d2f80159fd84114709a1101b9cabd8b8c
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-11-08 16:00:27 -0500

    move tests to run_test.py

---
+++ recipe/run_test.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+import pytest
+
+print("preflighting uvloop...", flush=True)
+try:
+    import uvloop
+    print("... uvloop _appears_ to be working!")
+except Exception as err:
+    print(f"... uvloop import error, ignoring:\n{err}")
+
+COV_THRESHOLD = "71"
+
+SKIPS = [
+    "connection_refused",
+    "happy_eyeballs",
+    "ipv6",
+]
+
+PYTEST_ARGS = [
+    "-vv",
+    "--cov", os.environ["PKG_NAME"],
+    "--cov-fail-under", COV_THRESHOLD,
+    "-k", f"""not ({" or ".join(SKIPS)})""",
+    "src"
+]
+
+print(">>> pytest", " ".join(PYTEST_ARGS), flush=True)
+
+sys.exit(pytest.main(PYTEST_ARGS))
